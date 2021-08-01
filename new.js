8dd05c924066a3ee7b2c14b9ee910514cd283666
johnwhiteweb <john@whiteweb.co.il>
2021-08-01 14:23:18 +0300

    up

--- new.js
+++ new.js
@@ -5,44 +5,61 @@ jQuery(document).ready(function ($) {
 
     $('#body1').contents().text(function () {
         var htmlString = $(this).html();
-        if (!htmlString) { return }
-        if (htmlString.slice(0, 1) == 0 || htmlString.slice(0, 1) == " ") {
+        if (!$(this).html()) { return }
+        if ($(this).html().slice(0, 1) == 0 || $(this).html().slice(0, 1) == " ") {
 
+            y = htmlString.split(' ').filter(w => w !== '');
+            
+            // split list words - if a word begins with a number - mark it
             // setter !
-            validate(htmlString);
-            console.log(this);
+            testInfo(y);
             // once completed clean up - activate:
             //     $( this).wrap(function() {
-            //     return "<a href='tel:" + $( this ).text() + "'></a>";
-            //   });
-        }
+                //     return "<a href='tel:" + $( this ).text() + "'></a>";
+                //   });
 
-        function validate(x) {
-            y = x.split(' ').filter(w => w !== '');
-            function testInfo(y) {
+                // y.forEach(i => {
+                //     testInfo(i)
+                // });
 
-                var re = /^(?:0(?!5)(?:2|3|4|8|9))(?:-?\d){7}$|^(0(?=5)(?:-?\d){9})$/;
-                var OK = re.exec(y);
-                if (!OK) {
+            }
+            
+                
+                
+                function testInfo(x) {
+                console.log(x);
+               
+                const mobile = /^(?:0(?!5)(?:2|3|4|8|9))(?:-?\d){7}$|^(0(?=5)(?:-?\d){9})$/;
+                // const lendPhone = /^(?:0(?:2|3|4|8|9))(?:-?\d){8}$|^(0(?:-?\d){9})$/;
+
+                let OK = mobile.exec(y);
+                // let OKL = lendPhone.exec(y);
+                
+                if(OK == null){return}
+                
+                
+                if (!OK ) {
                     // console.log('not a match', OK);
                 } else {
-                    console.log(`number is ${OK[0]}`);
-
-                    var newhtml = OK[0].replace(/href=['"]callto:/gi, 'href="tel:')
-                    console.log(newhtml);
+                    console.log(`mobile number is ${OK[0]}`);
+                    // console.log(`land number is ${OKL[0]}`);
+                 
+                    OK[0].replace(/href=['"]callto:/gi, 'href="tel:')
+                    // OKL[0].replace(/href=['"]callto:/gi, 'href="tel:')
+                    
      
-            }
-                y.forEach(i => {
-                    testInfo(i)
-                });
-            }
+            
+        }
 
 
-        });
+
+        };
 
 
 
 
 });
+});
+
 
 
